c3b9dfb9c1e1ec20a54cb41b6cd30406f07dade9
David Zuckerman <davidzuckerman@Davids-iMac.local>
2022-05-16 11:39:50 -0700

    changed rspec for changed testfile name

--- spec/gobi_spec.rb
+++ spec/gobi_spec.rb
@@ -1,11 +1,12 @@
 # spec/gobi_spec.rb
 require 'pathname'
+require 'date'
 require 'gobi'
 describe GOBI do
   let(:incoming_file) { './data/incoming/ebook0223.ord' }
   let(:processed_incoming_file) { './data/incoming/processed/ebook0223.ord' }
-  let(:processed_DEG_file) { './data/gobi_processed/ebookDEG0223.ord' }
-  let(:processed_EBS_file) { './data/gobi_processed/ebookEBS0223.ord' }
+  let(:processed_DEG_file) { "./data/gobi_processed/ebookDEG#{Date.today.year}0223.ord" }
+  let(:processed_EBS_file) { "./data/gobi_processed/ebookEBS#{Date.today.year}0223.ord" }
 
   it 'seeds data incoming with fixture data' do
     GOBI.seed!
